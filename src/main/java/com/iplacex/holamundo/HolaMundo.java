c0a4e6e38749b2c1b9f3177fc5973628f7fd927d
Rodrigo <rodrigocort98@gmail.com>
2025-01-20 23:09:40 -0300

    Subiendo proyecto holaMundo

---
+++ src/main/java/com/iplacex/holamundo/HolaMundo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.iplacex.holamundo;
+
+/**
+ *
+ * @author rodri
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo desde Jenkins!!!!");
+    }
+}
